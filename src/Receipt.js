48c4701ab5b6192ab8fa071b2744511a44d35a33
Shahrear ahamed <its.shahrear@gmail.com>
2022-07-28 11:57:55 +0600

    make receipt component and add custom style

--- src/Receipt.js
+++ src/Receipt.js
@@ -1,9 +1,48 @@
 import React from "react";
 
 const Receipt = () => {
+  const paymentMode = ["Cash", "Bank", "Cheque", "Paytm"];
   return (
     <div>
-      <h1>this is Receipt js file</h1>
+      <h4 className="receipt-title">Receipt Details</h4>
+      <form className="receipt-form">
+        <div className="input-div">
+          <label htmlFor="" className="input-label">Date</label>
+          <input type="number" name="date" id="date" className="input-field" placeholder="Enter Date" />
+        </div>
+        <div className="input-div">
+          <label htmlFor="" className="input-label">Amount</label>
+          <input
+            type="number"
+            name="date"
+            id="date"
+            className="input-field"
+            placeholder="Enter Amount (in INR)"
+          />
+        </div>
+        <div className="input-div">
+          <label htmlFor="" className="input-label" >Payment Mode</label>
+          <select name="" id="">
+            {paymentMode.map((mode, i) => (
+              <option key={i} value="">
+                {mode}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="input-div">
+          <label htmlFor="" className="input-label">Remark</label>
+          <input
+            type="number"
+            name="date"
+            id="date"
+            className="input-field"
+            placeholder="Enter Remark"
+          />
+        </div>
+
+        {/* submit button  */}
+      </form>
     </div>
   );
 };
